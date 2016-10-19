ed9cd2cbfab918f39a8d0e750ed732f2130610c4
Steve Bennett <steve@electricpocket.com>
2016-10-19 15:01:02 +0100

    g

--- Software/Python/grove_accelerometer_16g/rpy_example.py
+++ Software/Python/grove_accelerometer_16g/rpy_example.py
@@ -32,7 +32,7 @@ heave=0;sway=0;surge=0;rollsum=0;pitchsum=0;pitchmax=0;rollmax=0;surgemax=0;heav
 heaveV=0;swayV=0;surgeV=0;surgesum=0;heavesum=0;swaysum=0
 count=0; 
 while True:
-    axes = adxl345.getAxes(True)
+    axes = adxl345.getAxes(False)
     pitch= 180*(math.atan2(-axes['y'],axes['z']))/math.pi
     roll = 180*(math.atan2(-axes['x'],(axes['y']*axes['y']+axes['z']*axes['z'])))/math.pi
     heave=axes['z']
@@ -64,16 +64,18 @@ while True:
         #see https://docs.google.com/document/d/1P1K23f8aAzeZkK1TB_iIkhLFMHQiSzMuK3u-V7evQaM/edit?usp=sharing
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(server_address)
-        msg = 'P' + 'PMR' +'A,'+ timestamp+',' + '0'+','+ 'T'+','+ ("%.2f" %rollmax)+','+ str(pitchmax)+',' +str(heavemax)+',0,0,0,'+str(swaymax)+','+str(surgemax)+',0,0'
-        chksum=checksum(msg)
+        #msg = 'P' + 'PMR' +'A,'+ timestamp+',' + '0'+','+ 'T'+','+ ("%.2f" %rollmax)+','+ str(pitchmax)+',' +str(heavemax)+',0,0,0,'+str(swaymax)+','+str(surgemax)+',0,0'
+        #chksum=checksum(msg)
 
-        nmea='$' + msg+'*'+ ("%X" %chksum)+"\r\n"
-        jsonmsg = '{"timestamp":'+ timestamp+',"id":'+'7114'+',"rollmax":'+("%.2f" %rollmax)+',"pitchmax":'+ str(pitchmax)+',"heavemax":' +str(heavemax)+',"swaymax":'+str(swaymax)+',"surgemax":'+str(surgemax)+',"rollavg":'+("%.2f" %roll)+',"pitchavg":'+ str(pitch)+',"heave":' +str(heavesum)+',"sway":'+str(swaysum)+',"surge":'+str(surgesum)+'}'
+        #nmea='$' + msg+'*'+ ("%X" %chksum)+"\r\n"
+        jsonmsg = ('{"timestamp":'+ timestamp+',"id":'+'7114'+',"rollmax":'+("%.2f" %rollmax)+',"pitchmax":'+ str(pitchmax)+',"heavemax":' +
+                   str(heavemax)+',"swaymax":'+str(swaymax)+',"surgemax":'+str(surgemax)+',"rollavg":'+("%.2f" %roll)+',"pitchavg":'+ 
+                   str(pitch)+',"heave":' +str(heavesum)+',"sway":'+str(swaysum)+',"surge":'+str(surgesum)+'}')
         print jsonmsg 
         s.send(jsonmsg+"\r\n")
-        msg = 'P' + 'PMR' +'B,'+ timestamp+',' + '0'+','+ 'T'+','+ ("%.2f" %roll)+','+ str(pitch)+',' +str(heavesum)+',0,0,0,'+str(swaysum)+','+str(surgesum)+',0,0'
-        chksum=checksum(msg)
-        nmea= '$' + msg+'*'+ ("%X" %chksum)+"\r\n"
+        #msg = 'P' + 'PMR' +'B,'+ timestamp+',' + '0'+','+ 'T'+','+ ("%.2f" %roll)+','+ str(pitch)+',' +str(heavesum)+',0,0,0,'+str(swaysum)+','+str(surgesum)+',0,0'
+        #chksum=checksum(msg)
+        #nmea= '$' + msg+'*'+ ("%X" %chksum)+"\r\n"
 
         s.close()
         
