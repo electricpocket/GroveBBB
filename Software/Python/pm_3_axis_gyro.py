581c359d881b1a7613c65b2e9128cef7eb15156b
Steve Bennett <steve@electricpocket.com>
2016-10-24 15:04:31 +0100

    fix

--- Software/Python/pm_3_axis_gyro.py
+++ Software/Python/pm_3_axis_gyro.py
@@ -115,7 +115,7 @@ while True:
     gx =gx+sensor.x_offset/14.375
     gy = gy+sensor.y_offset/14.375
     gz= gz+sensor.z_offset/14.375
-    print ("%.2f" %(gx), ("%.2f" %(gy), ("%.2f" %(gz) , "deg/s"
+    print ("%.2f" %(gx)), ("%.2f" %(gy)), ("%.2f" %(gz)) , "deg/s"
     gxmax = max(gxmax,gx)
     gymax = max(gymax,gy)
     gzmax = max(gzmax,gz)
